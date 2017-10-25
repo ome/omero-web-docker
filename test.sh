e468e732778628136a8017ed87dcc5d78f6e1094
Simon Li <spli@dundee.ac.uk>
2017-10-25 12:54:35 +0100

    build standalone in travis

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ set -u
 
 PREFIX=test
 IMAGE=omero-web:$PREFIX
+STANDLONE=omero-web-standalone:$PREFIX
 
 CLEAN=${CLEAN:-y}
 
@@ -25,5 +26,18 @@ docker run -d --name $PREFIX-web \
     -e CONFIG_omero_web_debug=true \
     -p 4080:4080 \
     $IMAGE
+
+bash test_getweb.sh
+
+# Standalone image
+cleanup
+sed -i "s/FROM .*/FROM $IMAGE/" standalone/Dockerfile
+
+docker build -t $STANDLONE standalone/
+docker run -d --name $PREFIX-web \
+    -e CONFIG_omero_web_server__list='[["omero.example.org", 4064, "test-omero"]]' \
+    -e CONFIG_omero_web_debug=true \
+    -p 4080:4080 \
+    $STANDLONE
 
 bash test_getweb.sh
