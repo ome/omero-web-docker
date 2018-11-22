c20fd83fa3b352830805d17d56dfda064d28016a
Simon Li <spli@dundee.ac.uk>
2018-11-22 15:31:51 +0000

    add default web config to playbook

--- 60-default-web-config.sh
+++ 60-default-web-config.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-# Use omero.web.server_list if set, othen OMEROHOST if set, defaulting to a
-# single server of omero
-# Also default to binding web to all IPs
+# Override omero.web.server_list with OMEROHOST if set
 
 set -eu
 
 omero=/opt/omero/web/OMERO.web/bin/omero
 
-OMEROHOST=${OMEROHOST:-omero}
-CONFIG_omero_web_server__list=${CONFIG_omero_web_server__list:-}
-
-if [ -z "$CONFIG_omero_web_server__list" ]; then
+OMEROHOST=${OMEROHOST:-}
+if [ -n "$OMEROHOST" ]; then
     $omero config set omero.web.server_list "[[\"$OMEROHOST\", 4064, \"omero\"]]"
-fi
-
-CONFIG_omero_web_application__server_host=${CONFIG_omero_web_application__server_host:-}
-if [ -z "$CONFIG_omero_web_application__server_host" ]; then
-    $omero config set omero.web.application_server.host "0.0.0.0"
 fi
