d824777207a56c0442025a1743630346e39a188a
Simon Li <spli@dundee.ac.uk>
2019-09-23 15:46:25 +0100

    Activate venv instead of setting PATH

--- 99-run.sh
+++ 99-run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+echo "Activating virtualenv"
+. /opt/omero/web/venv/bin/activate
+
 set -eu
 
-export PATH="/opt/omero/web/venv/bin:$PATH"
-python=/opt/omero/web/venv/bin/python
 omero=/opt/omero/web/OMERO.web/bin/omero
 cd /opt/omero/web
 echo "Starting OMERO.web"
-exec $python $omero web start --foreground
+exec python $omero web start --foreground
