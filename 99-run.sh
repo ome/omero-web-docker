eade8037c556d0632fefd5b04da488fa251140d5
Simon Li <spli@dundee.ac.uk>
2019-09-23 15:46:56 +0100

    Start command can be changed

--- 99-run.sh
+++ 99-run.sh
@@ -7,5 +7,10 @@ set -eu
 
 omero=/opt/omero/web/OMERO.web/bin/omero
 cd /opt/omero/web
-echo "Starting OMERO.web"
-exec python $omero web start --foreground
+if [ $# -eq 0 ]; then
+    echo "Starting OMERO.web"
+    exec python $omero web start --foreground
+else
+    echo "execing custom command: $@"
+    exec "$@"
+fi
